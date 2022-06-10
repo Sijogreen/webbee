9037cca5bfdad4385fb7bc85bf7396630a9c3082
sijo <sijo.green@gmail.com>
2022-06-10 20:05:23 +0530

    GET/futureevents

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use Carbon\Carbon;
 
 class EventsController extends BaseController
 {
@@ -184,6 +185,11 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+
+        return Event::has('Workshops')->with(['Workshops' => function($query) {
+            $query->whereDate('start','>=', Carbon::now());
+          }])->get();
+
+        //throw new \Exception('implement in coding task 2');
     }
 }
